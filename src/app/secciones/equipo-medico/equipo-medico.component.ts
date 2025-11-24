bb133179d96008753a5a9184b9e317119e17e6c5
Lucas Amendola <lukitasromanpiles7@gmail.com>
2025-11-24 13:28:34 -0300

    feat: integrate small banner component and add image for 'Equipo Médico' section

--- src/app/secciones/equipo-medico/equipo-medico.component.ts
+++ src/app/secciones/equipo-medico/equipo-medico.component.ts
@@ -2,11 +2,12 @@ import { AfterViewInit, Component, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SimplySection } from '../../section-component/simply-section/simply-section';
 import { MedicoComponent } from '../../medico/medico.component';
+import { SmallBanner } from '../../small-banner/small-banner/small-banner';
 
 @Component({
   selector: 'app-equipo-medico',
   standalone: true,
-  imports: [SimplySection, CommonModule, MedicoComponent],
+  imports: [SimplySection, CommonModule, MedicoComponent, SmallBanner],
   templateUrl: './equipo-medico.component.html',
   styleUrl: './equipo-medico.component.css'
 })
