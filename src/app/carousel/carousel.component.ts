fe2aad8a1723177f1422932c14fe7043bd9dc9da
Lucas Amendola <lukitasromanpiles7@gmail.com>
2025-12-10 21:16:15 -0300

    feat: implement carousel component and integrate telemedicine sections in home page

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,68 @@
+import { Component, Input, OnInit, HostListener } from '@angular/core';
+import { NgFor, NgClass } from '@angular/common';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [NgFor, NgClass],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+export class CarouselComponent implements OnInit {
+
+  @Input() images: string[] = [];
+  @Input() autoPlay: boolean = true;
+  @Input() interval: number = 3000;
+
+  currentIndex = 0;
+  touchStartX = 0;
+  touchEndX = 0;
+  autoSlide: any;
+
+  ngOnInit() {
+    if (this.autoPlay) {
+      this.startAutoSlide();
+    }
+  }
+
+  startAutoSlide() {
+    this.autoSlide = setInterval(() => {
+      this.next();
+    }, this.interval);
+  }
+
+  stopAutoSlide() {
+    clearInterval(this.autoSlide);
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  goTo(index: number) {
+    this.currentIndex = index;
+  }
+
+  // Gestos mobile
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(e: TouchEvent) {
+    this.touchStartX = e.changedTouches[0].screenX;
+  }
+
+  @HostListener('touchend', ['$event'])
+  onTouchEnd(e: TouchEvent) {
+    this.touchEndX = e.changedTouches[0].screenX;
+    this.handleSwipe();
+  }
+
+  handleSwipe() {
+    const dist = this.touchStartX - this.touchEndX;
+    if (dist > 60) this.next();
+    if (dist < -60) this.prev();
+  }
+}
--- src/app/secciones/home/home.ts
+++ src/app/secciones/home/home.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { SimplySection } from '../../section-component/simply-section/simply-section';
 import { Servicios } from '../../servicios/servicios';
 import { ContactPage } from '../../contact-page/contact-page';
+import { LisSectionComponent } from '../../list-section/lis-section-component';
+import { CarouselComponent } from '../../carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
-  imports: [SimplySection, Servicios, ContactPage],
+  imports: [SimplySection, Servicios, ContactPage, LisSectionComponent, CarouselComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
   standalone: true,
