24ce82595131f5092ccca4101e72ae031181b414
Lucas Amendola <lukitasromanpiles7@gmail.com>
2026-01-12 21:05:30 -0300

    feat: implement router with in-memory scrolling for improved navigation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+  
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled'
+      })
+    )
+  ]
+});
